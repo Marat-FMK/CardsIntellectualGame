85cf184c60cd6609e8b121aa2104ae28bd498c35
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-12-06 18:36:15 +0300

    Initial Commit

---
+++ CardsIntellectualGame/CardsIntellectualGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsIntellectualGameApp.swift
+//  CardsIntellectualGame
+//
+//  Created by Marat Fakhrizhanov on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CardsIntellectualGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
