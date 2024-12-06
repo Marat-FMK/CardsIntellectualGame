35a64ed19d436d66a17657c167908f11765161bb
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-12-06 22:37:03 +0300

    fix struct

---
+++ CardsIntellectualGame/EditCard.swift
@@ -0,0 +1,18 @@
+//
+//  EditCard.swift
+//  CardsIntellectualGame
+//
+//  Created by Marat Fakhrizhanov on 06.12.2024.
+//
+
+import SwiftUI
+
+struct EditCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCard()
+}
