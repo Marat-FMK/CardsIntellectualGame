38ede1062e84995743c048c4565ff0097addb8b6
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-12-06 22:37:31 +0300

    work with load and save cards

--- CardsIntellectualGame/ContentView.swift
+++ CardsIntellectualGame/ContentView.swift
@@ -14,8 +14,12 @@ extension View {
    }
 
 struct ContentView: View {
+    
+    @State private var cards = [Card]()
+    
+    @Environment(\.accessibilityVoiceOverEnabled) var accessibilityVoiceOverEnabled
+    
     @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
-    @State private var cards = Array<Card>(repeating: .example, count: 10)
     
     @State private var timeRemaining = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -23,13 +27,15 @@ struct ContentView: View {
     @Environment(\.scenePhase) var scenePhase
     @State private var isActive = true
     
+    @State private var showingEditScreen = false
+    
         var body: some View {
             ZStack {
                 Image(decorative: "background") // Делаем  картинку нечитаемой в VoiceOver
                     .resizable()
                     .ignoresSafeArea()
                 VStack {
-                    Text("Time: \(timeRemaining)")
+                    Text("Time: \(timeRemaining)") // показывает таймер
                         .font(.largeTitle)
                         .foregroundStyle(.white)
                         .padding(.horizontal, 20)
@@ -40,7 +46,7 @@ struct ContentView: View {
                         ForEach(0..<cards.count, id: \.self) { index in
                             CardView(card: cards[index]) {
                                 withAnimation {
-                                    removeCard(at: index)
+                                    removeCard(at: index) // удаляем карточки с аимацией
                                 }
                             }
                             .allowsHitTesting(index == cards.count - 1) // Что бы при свайпе реагировала только верхняя карточка
@@ -49,7 +55,7 @@ struct ContentView: View {
                             
                         }
                     }
-                    .allowsHitTesting(timeRemaining > 0)
+                    .allowsHitTesting(timeRemaining > 0) // Отключать свайпы по карточкам если их нет
                     
                     if cards.isEmpty {
                         Button("Start Again", action: resetCards)
@@ -60,20 +66,61 @@ struct ContentView: View {
                     }
                     
                 }
-                if accessibilityDifferentiateWithoutColor {
+                
+                
+                VStack {
+                    HStack {
+                        Spacer()
+
+                        Button {
+                            showingEditScreen = true // Рисуем кнопку добавления нового вопроса, которая перекинет нас на другой экран 
+                        } label: {
+                            Image(systemName: "plus.circle")
+                                .padding()
+                                .background(.black.opacity(0.7))
+                                .clipShape(.circle)
+                        }
+                    }
+
+                    Spacer()
+                }
+                .foregroundStyle(.white)
+                .font(.largeTitle)
+                .padding()
+                
+                
+                if accessibilityDifferentiateWithoutColor || accessibilityVoiceOverEnabled {
                     VStack {
                         Spacer()
 
                         HStack {
-                            Image(systemName: "xmark.circle")
-                                .padding()
-                                .background(.black.opacity(0.7))
-                                .clipShape(.circle)
+                            Button {
+                                withAnimation {
+                                    removeCard(at: cards.count - 1)
+                                }
+                            } label: {
+                                Image(systemName: "xmark.circle")
+                                    .padding()
+                                    .background(.black.opacity(0.7))
+                                    .clipShape(.circle)
+                            }
+                            .accessibilityLabel("Wrong") // Прочитать как Вронг при Войс овере
+                            .accessibilityHint("Mark your answer as being incorrect.")
+
                             Spacer()
-                            Image(systemName: "checkmark.circle")
-                                .padding()
-                                .background(.black.opacity(0.7))
-                                .clipShape(.circle)
+
+                            Button {
+                                withAnimation {
+                                    removeCard(at: cards.count - 1)
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .padding()
+                                    .background(.black.opacity(0.7))
+                                    .clipShape(.circle)
+                            }
+                            .accessibilityLabel("Correct") // Прочитать как ответ правильный Коррект при Войс овере
+                            .accessibilityHint("Mark your answer as being correct.")
                         }
                         .foregroundStyle(.white)
                         .font(.largeTitle)
@@ -81,15 +128,16 @@ struct ContentView: View {
                     }
                 }
             }
-            .onReceive(timer) { time in
+            
+            .onReceive(timer) { time in // включить таймер
                 
                 guard isActive else { return } // Если фолс - тогда выйди из ф- ии и не вычитай таймер
                 
-                if timeRemaining > 0 {
+                if timeRemaining > 0 { // отнимать от начального времени секунду каждую секунду )
                     timeRemaining -= 1
                 }
             }
-            .onChange(of: scenePhase) {
+            .onChange(of: scenePhase) {// что делать если ушли в бекграунд - остановить таймер
                 if scenePhase == .active {
                     if cards.isEmpty == false { // Если не фоновый режим и массив не пустой - то таймер активен и отнимает по одной секунде от времени
                         isActive = true
@@ -98,21 +146,41 @@ struct ContentView: View {
                     isActive = false // иначе ничего не отнимает
                 }
             }
+            .sheet(isPresented: $showingEditScreen, onDismiss: resetCards, content: EditCards.init)
+            .onAppear(perform: resetCards)
         }
     
- func removeCard(at index: Int) {
+ func removeCard(at index: Int) { // удаляем по индексу карточку - индекс беерем в фор ич переборе
+     
+     guard index >= 0 else { return }
+     
         cards.remove(at: index)
      
      if cards.isEmpty {
-         isActive = false // не сможем больше отнять секунды таймера
+         isActive = false // не сможем больше отнять секунды таймера если пустой массив карточек
      }
     }
     
- func resetCards() {
-        cards = Array<Card>(repeating: .example, count: 10)
+// func resetCards() { // обновляем игру и устанавливаем значения в начальное состояние и запускам таймер
+//        cards = Array<Card>(repeating: .example, count: 10)
+//        timeRemaining = 100
+//        isActive = true
+//    }
+    
+    func resetCards() { // обновляем игру и устанавливаем значения в начальное состояние и запускам таймер
         timeRemaining = 100
         isActive = true
+        loadData() // обновлять всегда 
     }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
+    }
+    
 }
 
 #Preview {
